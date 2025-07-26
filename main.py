313899b0001c673ab5c42573843a08131f44cf43
amanda-montarroios <amontarroiosoliveira@gmail.com>
2025-07-26 18:33:39 -0300

    API connection + error exceptions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-#import requests
+import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 from PyQt5.QtCore import Qt 
 
@@ -9,9 +9,9 @@ class WeatherApp(QWidget):
         self.city_label = QLabel("City name: ", self)
         self.city_input = QLineEdit(self)
         self.get_weather_button = QPushButton("Get Weather", self)
-        self.temperature_label = QLabel("21°C", self)
-        self.emijo_label = QLabel("☀︎", self)
-        self.description_label = QLabel("Description", self)
+        self.temperature_label = QLabel(self)
+        self.emijo_label = QLabel(self)
+        self.description_label = QLabel(self)
         self.initUI()
 
     def initUI(self):
@@ -67,6 +67,57 @@ class WeatherApp(QWidget):
                            font-size: 50px;
                            }
                            """)
+
+        self.get_weather_button.clicked.connect(self.get_weather)
+
+    def get_weather(self):
+        api_key = "2186dc530c4a851db095d8494f5c7c3d"
+        city = self.city_input.text()
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            
+            if data["cod"] == 200:
+                self.display_weather(data)
+
+        except requests.exceptions.HTTPError as http_error:
+            match response.status_code:
+                case 400:
+                    self.display_error("Bad request:\nPlease check your city name")
+                case 401:
+                    self.display_error("Unauthorized:\nInvalid API key")
+                case 403:
+                    self.display_error("Forbidden:\nAcess denied")
+                case 404:
+                    self.display_error("Not found:\nCity not found")
+                case 500:
+                    self.display_error("Internal server error:\nPlease try again later")
+                case 502:
+                    self.display_error("Bad gateway:\nInvalid response from server")
+                case 503:
+                    self.display_error("Service Inavailable:\nServer down")
+                case 504:
+                    self.display_error("Gateway timeout:\nNo responses")
+                case _:
+                    self.display_error(f"HTTP error ocurred:\n{http_error}")
+
+        except requests.exceptions.ConnectionError:
+            self.display_error("Connection error:\nCheck your internet connection")      
+        except requests.exceptions.Timeout:
+            self.display_error("Timeout error")
+        except requests.exceptions.TooManyRedirects:
+            self.display_error("Too many redirects:\nCheck the url")
+        except requests.exceptions.RequestException as req_error:
+            self.display_error(f"Request error:\n{req_error}")
+
+    def display_error(self, message):
+        self.temperature_label.setText(message)
+    
+    def display_weather(self, data):
+        print(data)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
